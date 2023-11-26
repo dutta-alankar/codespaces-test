0524dadc212603d2d53bfc7e1fd6500e7f8e18c7
Alankar Dutta <dutta.alankar@gmail.com>
2023-11-26 06:25:40 +0000

    [ADD] Added own info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Alankar Dutta",
+  title: "Computational Astrophysicist",
+  email: "alankardutta@iisc.ac.in",
+  gitHub: "dutta-alankar",
+  instagram: "",
+  linkedIn: "",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
